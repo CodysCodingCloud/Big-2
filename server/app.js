5abc23b0ba597693b327dbaf30fa68a163d5adf1
codyWSL <adwingsx@aim.com>
2022-10-03 01:10:18 -0400

    morgan pro

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const morgan = require("morgan");
-app.use(morgan("dev"));
+if (process.env.NODE_ENV !== "production") {
+	const morgan = require("morgan");
+	app.use(morgan("dev"));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "..", "public")));
